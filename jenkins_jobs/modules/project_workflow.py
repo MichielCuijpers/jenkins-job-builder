9c5ca642faed90fa7cb642e55e13f115a1ca3f75
Joost van der Griendt <joostvdg@gmail.com>
2016-01-17 12:37:02 +0100

    Replacing David Caro's limited Workflow implementation. We do not use inline scripts, therefore we need to use the options for using external scripts from SCM. As we only use this via GIT, this is the only SCM for which I've added support. Someone who can do a better (read: proper) job implementing this, feel free to do so.

--- jenkins_jobs/modules/project_workflow.py
+++ jenkins_jobs/modules/project_workflow.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2015 David Caro <david@dcaro.es>
+# Copyright (C) 2015 Joost van der Griendt <joostvdg@gmail.com>
 #
 # Based on jenkins_jobs/modules/project_flow.py by
 # Copyright (C) 2013 eNovance SAS <licensing@enovance.com>
@@ -21,23 +22,34 @@
 The workflow Project module handles creating Jenkins workflow projects.
 You may specify ``workflow`` in the ``project-type`` attribute of
 the :ref:`Job` definition.
-For now only inline scripts are supported.
+
+You can add an SCM with the script-path, but for now only GIT is supported.
 
 Requires the Jenkins :jenkins-wiki:`Workflow Plugin <Workflow+Plugin>`.
 
 In order to use it for job-template you have to escape the curly braces by
-doubling them in the DSL: { -> {{ , otherwise it will be interpreted by the
+doubling them in the script: { -> {{ , otherwise it will be interpreted by the
 python str.format() command.
 
 :Job Parameters:
-    * **dsl** (`str`): The DSL content.
+    * **script** (`str`): The DSL content.
     * **sandbox** (`bool`): If the script should run in a sandbox (default
       false)
+    * **script-path** (`str`): The name and location of the DSL file to
+      execute as workflow.
 
-Job example:
+
+
+Job with inline script example:
 
     .. literalinclude::
       /../../tests/yamlparser/fixtures/project_workflow_template001.yaml
+
+Job with external script example:
+
+    .. literalinclude::
+      /../../tests/yamlparser/fixtures/project_workflow_template004.yaml
+
 
 Job template example:
 
@@ -45,29 +57,120 @@ Job template example:
       /../../tests/yamlparser/fixtures/project_workflow_template002.yaml
 
 """
+import logging
 import xml.etree.ElementTree as XML
-
-from jenkins_jobs.errors import MissingAttributeError
+from jenkins_jobs.errors import (JenkinsJobsException)
 import jenkins_jobs.modules.base
 
 
 class Workflow(jenkins_jobs.modules.base.Base):
     sequence = 0
 
-    def root_xml(self, data):
-        xml_parent = XML.Element('flow-definition',
-                                 {'plugin': 'workflow-job'})
-        xml_definition = XML.SubElement(xml_parent, 'definition',
-                                        {'plugin': 'workflow-cps',
-                                         'class': 'org.jenkinsci.plugins.'
-                                         'workflow.cps.CpsFlowDefinition'})
-        try:
-            XML.SubElement(xml_definition, 'script').text = data['dsl']
-        except KeyError as e:
-            raise MissingAttributeError(e.arg[0])
+def root_xml(self, data):
+        xml_parent = XML.Element('flow-definition')
+        xml_parent.attrib['plugin'] = 'workflow-job'
 
-        needs_workspace = data.get('sandbox', False)
-        XML.SubElement(xml_definition, 'sandbox').text = str(
-            needs_workspace).lower()
+        definition = XML.SubElement(xml_parent, 'definition')
+        definition.attrib['class'] = 'org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition'
+        definition.attrib['plugin'] = 'workflow-cps'
+
+        if 'workflow' not in data:
+            return xml_parent
+
+        if 'scm' in data['workflow']:
+            #self.registry.dispatch('scm', self, definition, data['workflow']['scm'])
+            if 'git' in data['workflow']['scm']:
+                self.git(definition, data['workflow']['scm']['git'])
+                definition.attrib['class'] = 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition'
+
+        if 'script' in data['workflow']:
+            XML.SubElement(definition, 'script').text = data['workflow'].get('script', '')
+            if 'sandbox' in data['workflow']:
+                needs_workspace = data['workflow'].get('sandbox', False)
+                XML.SubElement(definition, 'sandbox').text = str(needs_workspace).lower()
+
+        if 'script-path' in data['workflow']:
+            XML.SubElement(definition, 'scriptPath').text = data['workflow'].get('script-path', '')
+
 
         return xml_parent
+
+    def git(self, xml_parent, data):
+        logger = logging.getLogger("%s:workflow-git" % __name__)
+        scm = XML.SubElement(xml_parent,
+                             'scm', {'class': 'hudson.plugins.git.GitSCM'})
+        scm.attrib['plugin'] = 'git'
+        XML.SubElement(scm, 'configVersion').text = '2'
+
+        user = XML.SubElement(scm, 'userRemoteConfigs')
+
+        if 'remotes' not in data:
+            data['remotes'] = [{data.get('name', 'origin'): data.copy()}]
+
+        for remoteData in data['remotes']:
+            huser = XML.SubElement(user, 'hudson.plugins.git.UserRemoteConfig')
+            remoteName = next(iter(remoteData.keys()))
+            #XML.SubElement(huser, 'name').text = remoteName
+            remoteParams = next(iter(remoteData.values()))
+
+            if 'url' in remoteParams:
+                remoteURL = remoteParams['url']
+            else:
+                raise JenkinsJobsException('Must specify a url for git remote \"' +
+                                           remoteName + '"')
+            XML.SubElement(huser, 'url').text = remoteURL
+            if 'credentials-id' in remoteParams:
+                credentialsId = remoteParams['credentials-id']
+                XML.SubElement(huser, 'credentialsId').text = credentialsId
+
+        xml_branches = XML.SubElement(scm, 'branches')
+        branches = data.get('branches', ['**'])
+
+        for branch in branches:
+            bspec = XML.SubElement(xml_branches, 'hudson.plugins.git.BranchSpec')
+            XML.SubElement(bspec, 'name').text = branch
+
+        # add addition elements
+        #<doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>
+        #<submoduleCfg class="list"/>
+
+        mappingOld = [
+        # option, xml name, default value (text), attributes (hard coded)
+            (None, 'doGenerateSubmoduleConfigurations', False),
+            (None, 'submoduleCfg', '', {'class': 'list'}),
+        ]
+
+        mapping = [
+            # option, xml name, default value (text), attributes (hard coded)
+            ("ignore-notify", "ignoreNotifyCommit", False),
+            ("shallow-clone", "useShallowClone", False),
+        ]
+
+        # first adding the deprecated options
+        for elem in mappingOld:
+            (optname, xmlname, val) = elem[:3]
+            attrs = {}
+            attrs = {}
+            if len(elem) >= 4:
+                attrs = elem[3]
+            xe = XML.SubElement(scm, xmlname, attrs)
+            if optname and optname in data:
+                val = data[optname]
+            if type(val) == bool:
+                xe.text = str(val).lower()
+            else:
+                xe.text = val
+
+        exts_node = XML.SubElement(scm, 'extensions')
+        for elem in mapping:
+            (optname, xmlname, val) = elem[:3]
+            attrs = {}
+            if len(elem) >= 4:
+                attrs = elem[3]
+            xe = XML.SubElement(exts_node, xmlname, attrs)
+            if optname and optname in data:
+                val = data[optname]
+            if type(val) == bool:
+                xe.text = str(val).lower()
+            else:
+                xe.text = val
